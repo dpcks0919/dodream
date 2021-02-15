e86ccf25e025a653d24e0b5153b5fe3afd078ccf
yechankim <yechankim@172.17.196.83>
2021-02-15 14:34:40 +0900

    yc - userController request 수

--- src/main/java/com/dodream/controller/UserController.java
+++ src/main/java/com/dodream/controller/UserController.java
@@ -6,9 +6,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UserController {
 
-	@GetMapping("user/request")
+	@GetMapping("user/requestlist")
 	public String userTest() {
 		return "request/request_list";
+	}
+	
+	//	성민 테스트로 넣어놓은 부분 
+	@GetMapping("user/requestForm")
+	public String requestTest() {
+		return "request/request";
 	}
 
 }
