445d4c28c26443dbb2bf9ae554350daadfc6b1ec
SJChoi556 <21500733@handong.edu>
2021-02-18 09:14:55 +0900

    SJ_MAP추가

--- src/main/java/com/dodream/service/RequestService.java
+++ src/main/java/com/dodream/service/RequestService.java
@@ -1,12 +1,14 @@
 package com.dodream.service;
 
-import javax.transaction.Transactional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.dodream.config.auth.PrincipalDetails;
+import com.dodream.model.ClientType;
 import com.dodream.model.Request;
 import com.dodream.model.RequestItem;
 import com.dodream.repository.RequestItemRepository;
@@ -14,22 +16,42 @@ import com.dodream.repository.RequestRepository;
 
 @Service
 public class RequestService {
-	
+
 	@Autowired
 	private RequestRepository requestRepository;
-	
+
 	@Autowired
 	private RequestItemRepository requestItemRepository;
-	
+
 	@Transactional
 	public Request saveRequest(Request request, @AuthenticationPrincipal PrincipalDetails principalDetails) {
 		request.setUser(principalDetails.getUser());
+		System.out.println("saveRequest");
 		requestRepository.save(request);
 		return request;
 	}
-	
+
 	@Transactional
 	public void saveRequestItem(RequestItem requestItem, @AuthenticationPrincipal PrincipalDetails principalDetails) {
 		requestItemRepository.save(requestItem);
 	}
+
+	@Transactional
+	public Request[] markProcService(String clientType) {
+		
+		Request[] requestList = requestRepository.findAllByClientType(ClientType.valueOf(clientType));
+//		for (int i = 0; i < requestList.length; i++) {
+//			System.out.println(requestList[i].getRequestAddress());
+//		}
+		return requestList;
+	}
+	
+	@Transactional
+	public Request viewDetail(int id) {
+		return requestRepository.findById(id)
+				.orElseThrow(()->{
+					return new IllegalArgumentException("글 상세보기 실패 : 아이디를 찾을 수 없습니다.");
+				});
+	}
+	
 }
