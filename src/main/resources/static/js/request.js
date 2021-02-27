f7064293f8565aac7e0c99839c2c267c0e3af290
yechankim <yechankim@localhost>
2021-02-27 18:03:00 +0900

    YC_ request 수정

--- src/main/resources/static/js/request.js
+++ src/main/resources/static/js/request.js
@@ -10,6 +10,16 @@ var mapContainer = document.getElementById('map'), // 지도를 표시할 div
 
 var map = new kakao.maps.Map(mapContainer, mapOption);
 
+// 지도 확대, 축소 컨트롤에서 확대 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomIn() {
+	map.setLevel(map.getLevel() - 1);
+}
+
+// 지도 확대, 축소 컨트롤에서 축소 버튼을 누르면 호출되어 지도를 확대하는 함수입니다
+function zoomOut() {
+	map.setLevel(map.getLevel() + 1);
+}
+
 // 도로명주소 검색
 function goPopup(){
 	var pop = window.open("/jusoPopup_request","pop","width=570,height=420, scrollbars=yes, resizable=yes"); 
