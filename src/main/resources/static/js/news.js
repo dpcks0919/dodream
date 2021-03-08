7369deade86f0e0baf39e22580714983f28e22a4
21500706 <21500706@handong.edu>
2021-03-08 19:19:27 +0900

    sm_0308_myrequest

--- src/main/resources/static/js/news.js
+++ src/main/resources/static/js/news.js
@@ -5,13 +5,22 @@ function goNews(newsType) {
 }
 
 function newDetail(newsId) {
-	location.href="/user/news/newsDetail/"+newsId;
+	location.href="/news/newsDetail/"+newsId;
 }
 
 // 소식 목록 구성을 위해 사용.
-function news_upload() {
-	newsInit.uploadNews();
+function news_upload(type, id) {
+	if(type == 1) {
+		newsInit.uploadNews();		
+	} else if(type == 2) {
+		newsInit.updateNews(id);
+	}
 }
+
+function news_delete() {
+	alert('삭제합니다.');
+	
+} 
 
 let newsInit = {
 	init: function() {
@@ -24,7 +33,7 @@ let newsInit = {
 		} else if($("#news-content").val() == "") {
 			alert("내용을 입력해주세요.");
 		} else if(!$("#news-content").val().includes('<img src=')) {
-			alert("대표 이미지로 필요합니다. 이미지를 하나 이상 입력해주세요.");	
+			alert("대표 이미지가 필요합니다. 이미지를 하나 이상 입력해주세요.");	
 		}
 		else {
 			var newsType = "NEIGHBOR";
@@ -49,14 +58,55 @@ let newsInit = {
 				if(resp.status == 500) {
 					alert("업로드 실패");
 				} else {
-					alert("업로드 성공!");
-					location.href = "/user/news/newsList";
+					alert("새로운 소식이 등록되었습니다.");
+					location.href = "/news/newsList";
 					
 				}
 			}).fail(function(error) {
 				console.log(JSON.stringify(error));
 			});			
 		}
+	},
+	
+	updateNews:function(newsId) {
+		if($("#news-title").val() == "") {
+			alert("제목을 입력해주세요.");	
+		} else if($("#news-content").val() == "") {
+			alert("내용을 입력해주세요.");
+		} else if(!$("#news-content").val().includes('<img src=')) {
+			alert("대표 이미지가 필요합니다. 이미지를 하나 이상 입력해주세요.");	
+		}
+		else {
+			var newsType = "NEIGHBOR";
+			if ($("#news-type option:selected").text() == "두드림 소식") {
+				var newsType = "DODREAM";
+			}
+			let data = {
+				id : newsId,
+				title : document.getElementById('news-title').value,
+				content : document.getElementById('news-content').value,
+				newsType : newsType
+			};
+			$.ajax({
+				type:"POST",
+				url: "/newsUpdateProc",
+				data: JSON.stringify(data),
+				contentType: "application/json; charset = utf-8 ",
+				dataType: "json"
+			}).done(function(resp) {
+				console.log(data);
+				console.log(resp);
+				if(resp.status == 500) {
+					alert("업로드 실패");
+				} else {
+					alert("소식이 수정되었습니다.");
+					location.href = "/news/newsList";
+				}
+			}).fail(function(error) {
+				console.log(JSON.stringify(error));
+			});			
+		}
 		
-	}
+	}	
+	
 }
