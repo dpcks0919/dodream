55dedf1ca2d0c61c274c5e1c0be03f307388dc59
SJChoi556 <21500733@handong.edu>
2021-03-10 16:15:35 +0900

    SJ_userInterest 추가/삭제 구현

--- src/main/java/com/dodream/repository/UserInterestRepository.java
+++ src/main/java/com/dodream/repository/UserInterestRepository.java
@@ -6,4 +6,8 @@ import com.dodream.model.UserInterest;
 
 public interface UserInterestRepository extends JpaRepository<UserInterest, Integer> {
 
+	UserInterest findByUserIdAndRequestId(int id, int id2);
+
+	boolean existsByUserIdAndRequestId(int userId, int requestId);
+
 }
