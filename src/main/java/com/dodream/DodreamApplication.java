6df260d4f18ed6b887df7b20eb4d56a3bd2265d8
yechankim <yechankim@localhost>
2021-03-16 17:31:37 +0900

    yC

--- src/main/java/com/dodream/DodreamApplication.java
+++ src/main/java/com/dodream/DodreamApplication.java
@@ -11,3 +11,24 @@ public class DodreamApplication {
 	}
 
 }
+
+//package com.dodream;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+//
+//@SpringBootApplication
+//public class DodreamApplication extends SpringBootServletInitializer {
+//
+//   @Override
+//   protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//      return application.sources(DodreamApplication.class);
+//   }
+//   
+//   public static void main(String[] args) {
+//      SpringApplication.run(DodreamApplication.class, args);
+//   }
+//
+//}
